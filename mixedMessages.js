643a9412555558d5d7f0b36035ebc4fb7cd88f43
icyDenev <icy.denev@gmail.com>
2023-07-20 16:15:46 +0300

    Pick Random Message Functionality

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,3 +3,5 @@
 let songTitles = ["Kaji na maika si", "Cvete moe", "Jovano, Jovanke", "Ad i rai", "Prujinata"];
 let songLength = ["3:15", "4:20", "2:30", "5:24", "3:15"];
 let songYear = ["2008", "2004", "1999", "2000", "2005"];
+
+let randomSong = songTitles[Math.floor(Math.random() * songTitles.length)];
