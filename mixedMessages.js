7b44bdadb0c0338d2716c6964542699e533c76c7
icyDenev <icy.denev@gmail.com>
2023-07-20 16:14:51 +0300

    Store Message Components

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,2 +1,5 @@
-// This random message generator will return Slavi Trifonov's best song titles.
+// This random message generator will return Slavi Trifonov's best song titles, the lenght of the song and the year it was released.
 
+let songTitles = ["Kaji na maika si", "Cvete moe", "Jovano, Jovanke", "Ad i rai", "Prujinata"];
+let songLength = ["3:15", "4:20", "2:30", "5:24", "3:15"];
+let songYear = ["2008", "2004", "1999", "2000", "2005"];
