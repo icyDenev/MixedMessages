ee8e06a5a98754f72895eff034d67c458caccea8
icyDenev <icy.denev@gmail.com>
2023-07-20 16:22:18 +0300

    Add YouTube Links To Songs

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,7 +3,10 @@
 let songTitles = ["Kaji na maika si", "Cvete moe", "Jovano, Jovanke", "Ad i rai", "Prujinata"];
 let songLength = ["3:15", "4:20", "2:30", "5:24", "3:15"];
 let songYear = ["2008", "2004", "1999", "2000", "2005"];
+let youTubeLinksToSongs = ['https://www.youtube.com/watch?v=9QZ5Y5Z3XgE', 'https://www.youtube.com/watch?v=9QZ5Y5Z3XgE', 'https://www.youtube.com/watch?v=9QZ5Y5Z3XgE', 'https://www.youtube.com/watch?v=9QZ5Y5Z3XgE', 'https://www.youtube.com/watch?v=9QZ5Y5Z3XgE'];
 
-let randomSong = songTitles[Math.floor(Math.random() * songTitles.length)];
+let randomSongIndex = Math.floor(Math.random() * songTitles.length);
 
-let combinedSongMessage = 'Title: ' + songTitles[randomSong] + '\nLength:' + songLength[randomSong] + '\nYear: ' + songYear[randomSong];
+let combinedSongMessage = 'Title: ' + songTitles[randomSongIndex] + '\nLength: ' + songLength[randomSongIndex] + '\nYear: ' + songYear[randomSongIndex] + '\nLink: ' + youTubeLinksToSongs[randomSongIndex];
+
+console.log('Today you should listen to this Slavi Trifonov song:\n' + combinedSongMessage);
